2db868838c7a9d5fa6d3c02d3002a91f873b3ae5
itzik034 <itzik.king034@gmail.com>
2026-09-05 21:29:31 +0300

    isVaid fix

--- Frontend/src/Components/PagesArea/Contact/Contact.tsx
+++ Frontend/src/Components/PagesArea/Contact/Contact.tsx
@@ -26,6 +26,10 @@ export function Contact() {
         }
 
         setErrors(newErrors);
+
+        if (isValid) {
+            console.log('Form submitted', { nameVal, phoneVal, messageVal });
+        }
     };
 
     return (
